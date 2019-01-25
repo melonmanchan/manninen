9f7b78514afc07e8c7a7f491709813bc293ee5cc
Matti Jokitulppo <melonmanchan@gmail.com>
2019-01-25 17:09:38 +0200

    temp

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const fetch = require('node-fetch')
+const { promisify } = require('util')
+const { IncomingWebhook } = require('@slack/client')
+const redis = require('redis')
 const cheerio = require('cheerio')
 const ILTALEHTI_URL = 'https://www.is.fi/haku/?query=Tuomas%20Manninen'
+
+const client = redis.createClient()
+const existsAsync = promisify(client.exists).bind(client)
+const setAsync = promisify(client.set).bind(client)
+
+const url = process.env.SLACK_WEBHOOK_URL
+const webhook = new IncomingWebhook(url)
 
 async function main() {
   const page = await fetch(ILTALEHTI_URL)
@@ -11,12 +21,26 @@ async function main() {
 
   const list = searchResults
     .map(function(index, element) {
-      return `https://www.is.fi/${$(element).attr('href')}`
+      return `https://www.is.fi${$(element).attr('href')}`
     })
     .get()
 
-  console.dir(list)
+  list.forEach(async link => {
+    const reply = await existsAsync(link)
+
+    if (!reply) {
+      console.log(`Persisting link ${link}`)
+      client.set(link, 1)
+
+      webhook.send(link, function(err, res) {
+        if (err) {
+          console.log('Error:', err)
+        } else {
+          console.log('Message sent: ', res)
+        }
+      })
+    }
+  })
 }
 
-main()
-// setInterval(main, 5000)
+setInterval(main, 1000)
