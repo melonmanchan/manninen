ec792854bef8dbbfbe06b69668ec82748243e098
Matti Jokitulppo <melonmanchan@gmail.com>
2020-05-16 13:08:40 +0300

    Refactor iltalehti API logic a bit

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const fetch = require("node-fetch");
 const { promisify } = require("util");
 const { IncomingWebhook } = require("@slack/client");
 const redis = require("redis");
+const uuid = require("uuid");
+
 const ILTALEHTI_URL =
-  "https://www.is.fi/api/search/Tuomas%20Manninen/kaikki/month/new/0/50";
+  "https://www.is.fi/api/search/tuomas%20manninen/kaikki/month/new/0/50/0/1621158953538";
 
 const client = redis.createClient(process.env.REDIS_URL);
 const existsAsync = promisify(client.exists).bind(client);
@@ -12,33 +14,47 @@ const setAsync = promisify(client.set).bind(client);
 const webhooksUrls = (process.env.SLACK_WEBHOOK_URLS || "").split(",");
 const webhooks = webhooksUrls.map((url) => new IncomingWebhook(url));
 
+const id = uuid.v4();
+const log = (msg, ...rest) => console.log(`[id:${id}] ${msg}`, rest);
+
 async function main() {
   const page = await fetch(ILTALEHTI_URL);
+
+  if (!page.ok) {
+    log("Err", await page.text());
+    return;
+  }
+
   const contents = await page.json();
 
-  await Promise.all([
-    contents.map(async (post) => {
-      const { id, title, srcUrl } = post;
-      const url = `https://is.fi${srcUrl}/art-${id}.html`;
-      const reply = await existsAsync(id);
+  for (let i = 0; i < contents.length; i++) {
+    const post = contents[i];
+    log("Handling post", post);
 
-      if (!reply) {
-        console.log(`Persisting link ${url}`);
-        await setAsync(id, 1);
-        webhooks.forEach((w) => {
-          w.send(`${title} - ${url}`, function (err, res) {
-            if (err) {
-              console.log("Error:", err);
-            } else {
-              console.log("Message sent: ", res);
-            }
-          });
+    const { id, title, srcUrl } = post;
+    const url = `https://is.fi${srcUrl}/art-${id}.html`;
+    const reply = await existsAsync(id);
+
+    if (!reply) {
+      log(`Persisting link ${url}`);
+
+      await setAsync(id, 1);
+
+      webhooks.forEach((w) => {
+        w.send(`${title} - ${url}`, function (err, res) {
+          if (err) {
+            log("Error:", err);
+          } else {
+            log("Message sent: ", res);
+          }
         });
-      }
-    }),
-  ]);
+      });
+    } else {
+      log(`Post with id ${id} already exists`);
+    }
+  }
 
-  console.log("done");
+  console.log("Run done");
 
   process.exit(0);
 }
